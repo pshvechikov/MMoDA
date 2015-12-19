a7d292f50d965042eb03f2b8a6044947787bd960
Pavel Shvechikov <1xolodec@gmail.com>
2015-12-19 21:20:03 +0300

    Tiny fixes

--- R-files/Mirkin7.r
+++ R-files/Mirkin7.r
@@ -16,17 +16,16 @@ online.news.popularity <- online.news.popularity[,-c(1,2,36)]
 
 data <- subset(online.news.popularity, n_tokens_content > 0)
 
-data <- transform(data, num_hrefs = log(num_hrefs), num_self_hrefs = log(num_self_hrefs),
+data <- transform(data, 
+                  num_hrefs = log(num_hrefs), 
+                  num_self_hrefs = log(num_self_hrefs),
                   self_reference_avg_sharess = log(self_reference_avg_sharess),
-                  shares = log(shares), n_tokens_content = log(n_tokens_content))
-
+                  shares = log(shares), 
+                  n_tokens_content = log(n_tokens_content),
+                  num_imgs = log(num_imgs),
+                  num_videos = log(num_videos))
 data[data == -Inf] = -2
-
-
-data.nrm <- as.data.frame(apply(data, MARGIN = 2,  function(x) {
-  y <- (x - mean(x))/sd(x)
-  y
-}))
+data.nrm <- scale(data)
 
 
 
@@ -47,13 +46,23 @@ km <- function(data, num_cl, maxIter) {
 }
 
 ind <- sample(1:nrow(data.nrm), 1000)
+polarity_features <- c(25:32)
+num_of_words_feautures <- c(2:4, 9:10)
+channel_feature <- c(11:16)
+hrefs_and_images_features <- c(5:8)
+images_video_features  <-  c(7:8)
 
-data <- data.nrm[ind, c(1:24,33,34)]
-data <- data.nrm[ind, 25:32]
+features_to_cluster <- images_video_features
+features_to_cluster <- hrefs_and_images_features
+features_to_cluster <- channel_feature
+features_to_cluster <- num_of_words_feautures
+features_to_cluster <- polarity_features
 
 
+colnames(data.nrm)[features_to_cluster]
+data <- data.nrm[ind, features_to_cluster]
 
-k_mean_4 <- km(data, 3, 20)
+k_mean_3 <- km(data, 3, 20)
 pca.std <- prcomp(data, center = FALSE, scale. = FALSE)
 plot(pca.std$x[,1], pca.std$x[,2], col = k_mean_3$cluster, xlab = 'PCA 1', ylab = 'PCA 2', main = 'PCA: Z-score')
 
@@ -82,7 +91,7 @@ plot(get_errors_for_list_of_k(data, 50, 1:20),
      ylab="within-cluster sum of squares")
 
 
-##########################
+  ##########################
 ### 2. NN MST          ###
 ##########################
 
